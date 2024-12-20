1ecc0eb29ad8f700feea11dfbc7297bd46f85185
Rtanjay <rtanjayshrivastava.work@gmail.com>
2024-12-20 20:55:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ st.title('Prediction with Machine Learning')
 st.info('This is a prediction App !')
 
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+with st.expander("Data"):
+  st.write("**Raw Data** :")
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
 
