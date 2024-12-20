305b467f575eaa40fdaeae023784247b4f057bcd
Rtanjay <rtanjayshrivastava.work@gmail.com>
2024-12-20 20:51:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Prediction with Machine Learning')
 
 st.info('This is a prediction App !')
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
+
