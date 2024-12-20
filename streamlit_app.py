14c01a3061ba67c01c0cad73a57ca857741d881f
Rtanjay <rtanjayshrivastava.work@gmail.com>
2024-12-20 22:23:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ data = ({'island': island ,
 input_df = pd.DataFrame(data , index = [0])
 
 # combining the input datset with raw data
-input_penguins = pd.concat([x,input_df], axis = 0 , index = 0)
+input_penguins = pd.concat([input_df,x], axis = 0)
 
 with st.expander("Input Data"):
   st.write("**Input Data** :")
